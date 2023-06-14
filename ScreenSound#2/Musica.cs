6a2043e6de445afdca809519ae58a9fda0746d2d
Rafael "Zipão" Silva <zipao.silva@gmail.com>
2023-06-13 21:33:07 -0300

    aula1.5

--- ScreenSound#2/Musica.cs
+++ ScreenSound#2/Musica.cs
@@ -4,4 +4,18 @@
     public string artista;
     public int duracao;
     public bool disponivel;
+
+    public void ExibirFichaTecnica()
+    {
+        Console.WriteLine($"Nome: {nome}");
+        Console.WriteLine($"Artista: {artista}");
+        Console.WriteLine($"Duração: {duracao}");
+        if (disponivel)
+        {
+            Console.WriteLine("Disponível no plano.");
+        } else
+        {
+            Console.WriteLine("Adquiri o plano Plus+");
+        }
+    }
 }
