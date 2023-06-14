db81669b081b789ecb2f2eb61e7dbe88e231b4d6
Rafael "Zipão" Silva <zipao.silva@gmail.com>
2023-06-13 21:26:02 -0300

    aula1.4

--- ScreenSound#2/Program.cs
+++ ScreenSound#2/Program.cs
@@ -1,4 +1,11 @@
-﻿//nome
-//artista
-//duração
-//disponíuvel
+﻿Musica musica1 = new Musica();
+musica1.nome = "Roxane";
+musica1.artista = "The Police";
+
+Console.WriteLine($"Nome: {musica1.nome} \nArtista: {musica1.artista}");
+
+Musica musica2 = new Musica();
+musica2.nome = "Vertigo";
+musica2.artista = "U2";
+
+Console.WriteLine($"Nome: {musica2.nome} \nArtista: {musica2.artista}");
