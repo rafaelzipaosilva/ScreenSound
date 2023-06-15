630ccd08995e59e7a9d67c806a0ce1d3244b4801
Rafael "Zipão" Silva <zipao.silva@gmail.com>
2023-06-14 23:08:32 -0300

    podcast2

--- Podcast/Program.cs
+++ Podcast/Program.cs
@@ -1,5 +1,13 @@
-﻿Episodio ep1 = new(1, "Técnicas de facilicitação", 45);
+﻿Episodio ep1 = new(2, "Técnicas de facilicitação", 45);
 ep1.AdicionarConvidados("Maria");
 ep1.AdicionarConvidados("Marcelo");
 
-Console.WriteLine(ep1.Resumo);
+Episodio ep2 = new(1, "Técnicas de aprendizado", 67);
+ep2.AdicionarConvidados("Fernando");
+ep2.AdicionarConvidados("Marcos");
+ep2.AdicionarConvidados("Flávia");
+
+Podcast podcast = new("Podcast Especial", "Zipão");
+podcast.AdiconarEpisodio(ep1);
+podcast.AdiconarEpisodio(ep2);
+podcast.ExibirDetalhes();
